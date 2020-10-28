d2d4d92c341887304b51dbff8a2e980e965ed581
Shuai <shuaixie@zju.edu.cn>
2020-10-28 18:33:37 +0800

    add semantic_tree

--- demo/demo_alert.py
+++ demo/demo_alert.py
@@ -24,14 +24,13 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 import cv2
 import numpy as np
-import pandas as pd
 
 from datasets.config.railway import *
 from demo.demo_seg import segment, load_seg_model
 from demo.demo_dec import detect, load_dec_model, DecDecoder
+from demo.base import *
 from utils.func_utils import preprocess, plt_results
-from utils.misc import plt_compare_seg, box_major_axis, pt2line_distance
-from pprint import pprint
+from utils.misc import plt_compare_seg, box_major_axis
 
 
 class RailwayAlert:
@@ -44,120 +43,98 @@ class RailwayAlert:
         self.seg_classes = 7
         self.dec_classes = 17
 
-        # self.seg_model = load_seg_model()
+        self.seg_model = load_seg_model()
         self.dec_model = load_dec_model()
 
         self.decoder = DecDecoder(K=500, conf_thresh=0.18, num_classes=self.dec_classes)
 
-    @staticmethod
-    def parse_box_info(pred):
-        """
-        :param pred: (9,) 4点坐标 + score
-        """
-        score = pred[-1]
-        tl = np.asarray([pred[0], pred[1]], np.float32)
-        tr = np.asarray([pred[2], pred[3]], np.float32)
-        br = np.asarray([pred[4], pred[5]], np.float32)
-        bl = np.asarray([pred[6], pred[7]], np.float32)
+    def load_img(self, img_path):
+        self.ori_image = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+        self.ori_h, self.ori_w, _ = self.ori_image.shape
+        self.img = preprocess(self.ori_image, self.input_w, self.input_h).cuda()
 
-        box = np.asarray([tl, tr, br, bl], np.float32)
-        cen_pts = np.mean(box, axis=0)
-
-        return box, cen_pts, score
-
-    def dec_alert(self, test_path, vis=False):
+    def dec_alert(self, vis=False):
         """
         基于目标检测的态势评估
             根据场景中活动类别 到 train/rail 距离，提出预警
-        :param test_path: 测试图片路径
         :return: 态势评估结果
         """
-        ori_image = cv2.imread(test_path)
-        ori_h, ori_w, _ = ori_image.shape
-        img = preprocess(ori_image, self.input_w, self.input_h).cuda()
-
-        dec_results = detect(self.dec_model, img, self.decoder,
-                             self.input_w, self.input_h, ori_w, ori_h)
-
+        # self.load_img(img_path=test_path)
+        dec_results = detect(self.dec_model, self.img, self.decoder,
+                             self.input_w, self.input_h, self.ori_w, self.ori_h)
+        dec_results = {
+            cat: dec_results[cat]
+            for cat in dec_results if len(dec_results[cat]) > 0
+        }
         if vis:
-            plt_results(dec_results, ori_image, vis=False, save_path=test_path.replace('.png', '_det.png'))
+            plt_results(
+                dec_results, self.ori_image, vis=False,
+                save_path=test_path.replace('.png', '_det.png')
+            )
 
         # 预警类，取物体中心
         alter_cls = ['small-vehicle', 'large-vehicle', 'helicopter', 'plane', 'ship']  # 可活动类，潜在危险
         alter_objs = {'cen_pts': [], 'objs': []}
 
         for cat in alter_cls:
+            if cat not in dec_results:
+                continue
             result = dec_results[cat]  # n,9
             for idx, pred in enumerate(result):
-                box, cen_pts, score = self.parse_box_info(pred)
+                box, cen_pts, score = parse_box_info(pred)
                 alter_objs['cen_pts'].append(cen_pts)
                 alter_objs['objs'].append(f'{cat}_{idx + 1}')  # 直接编排同类物体
 
         # 如果场景中没有预警类出现，直接 status 正常
         if len(alter_objs['objs']) == 0:
-            return True, '正常', ''
+            return {
+                'dec_results': dec_results,
+                'status': True,
+                'assesement': '正常',
+                'info': ''
+            }
 
         # 保护类，取直线方程
         protect_cls = ['train', 'rail']
         protect_objs = {'lines': [], 'objs': []}
 
         for cat in protect_cls:
+            if cat not in dec_results:
+                continue
             result = dec_results[cat]
             for idx, pred in enumerate(result):
-                box, cen_pts, score = self.parse_box_info(pred)
+                box, cen_pts, score = parse_box_info(pred)
                 protect_objs['lines'].append(box_major_axis(box))
                 protect_objs['objs'].append(f'{cat}_{idx + 1}')
 
         # 如果场景中没有 要保护的类出现，直接 status 正常
         if len(protect_objs['lines']) == 0:
-            return True, '正常', ''
+            return {
+                'dec_results': dec_results,
+                'status': True,
+                'assesement': '正常',
+                'info': ''
+            }
 
         # alert-protect 类同时出现时，逐对比较，对场景状况进行判断
-        status, assesement, info = self.dist_judge(alter_objs, protect_objs)
-        return status, assesement, info
+        status, assesement, info = dist_judge(alter_objs, protect_objs)
 
-    @staticmethod
-    def dist_judge(alter_objs, protect_objs):
-        """
-        计算 alert_obj 到 protect_obj 距离，给出详细状态说明
-        :param alter_objs:
-        :param protect_objs:
-        :return: situation = {} 状态说明
-        """
+        return {
+            'dec_results': dec_results,  # 原始检测结果
+            'status': status,  # 态势评估，True 正常，False 不正常
+            'assesement': assesement,  # 态势评估 文字描述
+            'info': info,  # 详细信息
+        }
 
-        alert_cens = alter_objs['cen_pts']
-        protect_lines = protect_objs['lines']
-
-        w = len(alert_cens)
-        h = len(protect_lines)
-
-        distances = []
-
-        for line in protect_lines:  # row
-            for cen_pts in alert_cens:  # col
-                distances.append(pt2line_distance(cen_pts, line))
-
-        distances = np.array(distances).reshape((h, w))
-
-        df = pd.DataFrame(distances)
-        df.columns = alter_objs['objs']
-        df.index = protect_objs['objs']
-
-        return False, '异常', df
-
-    def seg_alert(self, base_path, test_path, vis=False):
+    def seg_alert(self, base_path, vis=False):
         """
         基于语义分割的态势评估
             根据测试场景 各类 iou 相较正常状态的变化，给出评估
         :param base_path: 正常情况的基础图像分割结果，作为 target
-        :param test_path: 测试图片
         :return: 态势评估结果
         """
         target = cv2.imread(base_path, cv2.IMREAD_UNCHANGED)
-
-        ori_image = cv2.imread(test_path)
-        img = preprocess(ori_image, self.input_w, self.input_h).cuda()
-        pred = segment(self.seg_model, img)
+        pred = segment(self.seg_model, self.img)
 
         if vis:
             plt_compare_seg(target, pred, seg_label_colors)
@@ -165,7 +142,7 @@ class RailwayAlert:
         situation = {}
 
         for i in range(1, self.seg_classes + 1):
-            status_ok, info, iou, less_percent, more_percent = self.iou_judge(target, pred, cls_idx=i)
+            status_ok, info, iou, less_percent, more_percent, cls_percent = iou_judge(target, pred, cls_idx=i)
             cat = seg_label_names[i]
 
             if not status_ok:
@@ -196,78 +173,103 @@ class RailwayAlert:
                 'status': status_ok,
                 'assessment': assessment,
                 'info': info,
-                'iou': iou
+                'iou': iou,
+                'proportion': cls_percent,
             }
 
-        judge = all([s['status'] for s in situation.values()])  # 全正常为 True
-        summary = [s['assessment'] for s in situation.values() if not s['status']]
+        status = all([s['status'] for s in situation.values()])  # 全正常为 True
+        assessment = [s['assessment'] for s in situation.values() if not s['status']]
+        seg_results = {
+            seg_map_zh[cat]: situation[cat]['proportion']  # 有效场景占比
+            for cat in situation if situation[cat]['proportion'] > 0.05
+        }
+        return {
+            'seg_results': seg_results,
+            'status': status,
+            'assessment': assessment,
+            'info': situation,  # 各类详细信息
+        }
 
-        pprint(situation)
-        print(summary)
-        print(judge)
-
-        return judge
-
-    @staticmethod
-    def iou_judge(target, pred, cls_idx, thre=0.1):
+    # @staticmethod
+    def semantic_tree(self, seg_results, dec_results):
         """
-        根据各类 iou 相较正常状态的变化，给出评估
-        :param target: 以存储的正常情况下 img_seg 结果作为 target
-        :param pred: 测试异常图片的预测的结果
-        :param cls_idx:
-        :param thre: 增减阈值判断
+        :param seg_results: 出现类别，及 占比
+            {'rail': 0.17600694444444445, 'plant': 0.5232851080246913, 'land': 0.1841184413580247, 'train': 0.06840277777777778}
+        :param dec_results: 各类 box, 前 8 个 为 4点坐标; 最后1个为 score
+            {'train': [array([8.72206299e+02, 1.28269379e+02, 8.75129395e+02, 1.61081055e+02,
+                              8.57380371e+01, 2.35098236e+02, 8.28148804e+01, 2.02286530e+02, 2.17964023e-01])]}
         :return:
+            Tree
         """
-        gt_mask = (target == cls_idx).astype(int)
-        pred_mask = (pred == cls_idx).astype(int)
+        treedict = {}
+        First_heading = '轨道交通语义结构树'
+        treedict[First_heading] = {}
 
-        gt_sum = gt_mask.sum()
-        pred_sum = pred_mask.sum()
+        print(seg_results)
+        print(dec_results)
 
-        # 抑制噪声
-        inter = (gt_mask * pred_mask).sum()
-        union = gt_sum + pred_sum - inter
+        for classname in seg_results.keys():
+            if classname in ['绿植', '房屋', '黄土地', '水域']:
+                if '地貌' not in treedict[First_heading]:
+                    treedict[First_heading]['地貌'] = {}
+                treedict[First_heading]['地貌'][classname] = 1
+            if classname in ['轨道']:
+                if '道路' not in treedict[First_heading]:
+                    treedict[First_heading]['道路'] = {}
+                treedict[First_heading]['道路'][classname] = 1
 
-        # iou judge
-        iou = (inter + 1) / (union + 1)
-        if iou > 0.7:
-            status_ok = True
-        else:
-            status_ok = False
+        for classname in dec_results.keys():
+            if classname in ['bridge', 'rail']:
+                count = len(dec_results[classname])
+                if '道路' not in treedict[First_heading]:
+                    treedict[First_heading]['道路'] = {}
+                if classname == 'bridge':
+                    classname = '桥梁'
+                    treedict[First_heading]['道路'][classname] = count
+                if classname == 'rail':
+                    classname = '轨道'
+                    treedict[First_heading]['道路'][classname] = count
+            elif classname in ['plane', 'ship', 'helicopter', 'small-vehicle', 'large-vehicle', 'train']:
+                if '交通工具' not in treedict[First_heading]:
+                    treedict[First_heading]['交通工具'] = {}
+                if classname in ['plane', 'ship', 'helicopter']:
+                    count = len(dec_results[classname])
+                    if classname == 'plane':
+                        classname = '飞机'
+                    if classname == 'ship':
+                        classname = '船只'
+                    if classname == 'helicopter':
+                        classname = '直升机'
+                    treedict[First_heading]['交通工具'][classname] = count
+                elif classname in ['small-vehicle', 'large-vehicle']:
+                    count = len(dec_results[classname])
+                    if '车辆' not in treedict[First_heading]['交通工具']:
+                        treedict[First_heading]['交通工具']['车辆'] = {}
+                    if classname == 'small-vehicle':
+                        classname = '小车'
+                    if classname == 'large-vehicle':
+                        classname = '大车'
+                    treedict[First_heading]['交通工具']['车辆'][classname] = count
+                elif classname == 'train':
+                    if '火车' not in treedict[First_heading]['交通工具']:
+                        treedict[First_heading]['交通工具']['火车'] = {}
 
-        # 消减判断 平滑处理; 即便是不存在的类，也无妨
-        less_percent = (gt_sum - inter + 1) / (gt_sum + 1)
-        more_percent = (pred_sum - inter + 1) / (gt_sum + 1)
+                    for line in dec_results[classname]:
+                        trainkind = find_train_kind(line[:-1], self.ori_image)
+                        if trainkind not in treedict[First_heading]['交通工具']['火车']:
+                            treedict[First_heading]['交通工具']['火车'][trainkind] = 0
+                        treedict[First_heading]['交通工具']['火车'][trainkind] = treedict[First_heading]['交通工具']['火车'][trainkind] + 1
 
-        if gt_sum == 0 and pred_sum == 0:
-            less_percent, more_percent = 0, 0
+        print(treedict)
 
-        if cls_idx == 1:  # rail 轨道，异物侵线，造成轨道减少
-            if less_percent > 0.05:
-                status_ok = False
-
-        if cls_idx == 2:  # plant 植被，减少区域不超过 thre 正常
-            status_ok = (less_percent - more_percent) < thre
-
-        if cls_idx == 3 or cls_idx == 4 or cls_idx == 7:  # buildings, road, train 抑制噪声
-            if gt_sum < pred_mask.size * 0.02 or pred_sum < pred_mask.size * 0.02:
-                status_ok = True
-
-        if cls_idx == 5:  # land 土地，荒漠化增加区域不超过 0.1，正常
-            status_ok = (more_percent - less_percent) < thre
-
-        if cls_idx == 6:  # water 水域，变化区域不超
-            status_ok = abs(more_percent - less_percent) < thre
-
-        info = '原始区域减少 {:.2f}%, 增加 {:.2f}% 新区域'.format(less_percent * 100, more_percent * 100)
-
-        return status_ok, info, iou, less_percent, more_percent
+        return treedict
 
 
 if __name__ == '__main__':
     ra = RailwayAlert()
 
-    # seg_pairs = [
+    ## segment
+    # scene_pairs = [
     #     ('data/geo_hazard/1_轨道变形/1_ori_class.png', 'data/geo_hazard/1_轨道变形/1_hzd.png'),
     #     ('data/geo_hazard/2_水漫线路/1_ori_class.png', 'data/geo_hazard/2_水漫线路/1_hzd.png'),
     #     ('data/geo_hazard/3_植被退化/1_ori_class.png', 'data/geo_hazard/3_植被退化/1_hzd.png'),
@@ -277,17 +279,26 @@ if __name__ == '__main__':
     #     ('data/geo_hazard/5_异物侵线/3_ori_class.png', 'data/geo_hazard/5_异物侵线/3_hzd.png'),
     #     ('data/geo_hazard/5_异物侵线/4_ori_class.png', 'data/geo_hazard/5_异物侵线/4_hzd.png'),
     # ]
-    #
     # for base_path, test_path in seg_pairs:
     #     print(test_path)
     #     ra.seg_alert(base_path, test_path)
     #     print()
 
-    dec_imgs = [
-        'data/geo_hazard/6_汽车误入/1.png',
-        'data/geo_hazard/6_汽车误入/2.png',
-    ]
+    ## detect
+    # dec_imgs = [
+    #     'data/geo_hazard/6_汽车误入/1.png',
+    #     'data/geo_hazard/6_汽车误入/2.png',
+    # ]
+    # for test_path in dec_imgs:
+    #     print(test_path)
+    #     ra.dec_alert(test_path, vis=True)
 
-    for test_path in dec_imgs:
-        print(test_path)
-        ra.dec_alert(test_path, vis=True)
+    ## tree
+    scene_pairs = [
+        ('data/geo_hazard/5_异物侵线/3_ori_class.png', 'data/geo_hazard/5_异物侵线/3_hzd.png'),
+    ]
+    for base_path, test_path in scene_pairs:
+        ra.load_img(test_path)
+        seg_res = ra.seg_alert(base_path)
+        dec_res = ra.dec_alert()
+        ra.semantic_tree(seg_res['seg_results'], dec_res['dec_results'])
